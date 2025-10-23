d6b39ba089bb490dcc122bc8c4f4b28d02c921d6
Carlos Andrevis <candrevis1706@gmail.com>
2025-10-22 20:09:11 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
+      const response = await fetch("https://uls-95ku.onrender.com/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
